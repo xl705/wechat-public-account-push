e5d73a852037947a01eb14f3c911e7b65fbdb314
xl705 <81683091+xl705@users.noreply.github.com>
2022-09-06 13:11:25 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -6,13 +6,13 @@ export const config = {
   // 公众号APP_ID 
   // 建议不要填这里，请使用文档中github secret的方法进行保密配置，保护您的隐私安全。
   // 如果你非要填这里也行。脚本也能运行
-   appID: "wxa4fb488aa5236c21",
+   APP_ID: "wxa4fb488aa5236c21",
 
   // 公众号APP_SECRET 
   // 建议不要填这里，请使用文档中github secret的方法进行保密配置，保护您的隐私安全。
   // 如果你非要填这里也行。脚本也能运行
-   appsecret: "67c3de2085750c978683a5c43644bbf0",
-
+   APP_SECRET: "67c3de2085750c978683a5c43644bbf0",
+  TEMPLATE_Id: "zjVglIuCXo4CsraXWEio1iP7sZJ7IQEES9lDsgc4yOU",
   /**
    * 接收公众号消息的微信号，如果有多个，需要在[]里用英文逗号间隔，例如
    * [
@@ -28,22 +28,23 @@ export const config = {
    *   }, 
    * ]
    */
-  USERS: [
-    {
-      // 想要发送的人的名字
-      name: "老婆0",
-      // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: "oGsSK5jMAfjiZJUb2DiU5pKcGe90",
-      // 你想对他发送的模板消息的模板ID
-      useTemplateId: "BqxF6DeDfKkrtgerjjHqxWnmphbpLOF8B9yM1ldpMS8",
-      // 所在省份
-      province: "广东",
-      // 所在城市
-      city: "肇庆",
-      // 他点击详情后跳转的页面,你可以设置成微博的热榜，也可以设置成其他，网址一定要填对；不填对也没关系，随便你，会打不开而已。
-      openUrl: "https://wangxinleo.cn"
-    }, 
-  ],
+  USERS:["oGsSK5jMAfjiZJUb2DiU5pKcGe90"]
+//   USERS: [
+//     {
+//       // 想要发送的人的名字
+//       name: "老婆0",
+//       // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
+//       id: "oGsSK5jMAfjiZJUb2DiU5pKcGe90",
+//       // 你想对他发送的模板消息的模板ID
+//       useTemplateId: "BqxF6DeDfKkrtgerjjHqxWnmphbpLOF8B9yM1ldpMS8",
+//       // 所在省份
+//       province: "广东",
+//       // 所在城市
+//       city: "肇庆",
+//       // 他点击详情后跳转的页面,你可以设置成微博的热榜，也可以设置成其他，网址一定要填对；不填对也没关系，随便你，会打不开而已。
+//       openUrl: "https://wangxinleo.cn"
+//     }, 
+//   ],
 
   /**
    * 回调消息 相关，主要用来展示发送是否成功/失败的数据
