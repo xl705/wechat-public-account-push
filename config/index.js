9d2d4edbc5242d10b4ca3c3474f899942a03781d
xl705 <81683091+xl705@users.noreply.github.com>
2022-09-06 13:07:13 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -35,7 +35,7 @@ export const config = {
       // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: "oGsSK5jMAfjiZJUb2DiU5pKcGe90",
       // 你想对他发送的模板消息的模板ID
-      useTemplateId: "VsPwWpPzW4Rk5quqqjWFc1SEWg_pM8bMjuH0h2Jac0s",
+      useTemplateId: "BqxF6DeDfKkrtgerjjHqxWnmphbpLOF8B9yM1ldpMS8",
       // 所在省份
       province: "广东",
       // 所在城市
@@ -50,7 +50,7 @@ export const config = {
    */
 
   // 回调消息模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: "zjV1glIuCXo4Csr23aXWEio1iP74sZJ7IQEES95lDsg6c4yOU",
+  CALLBACK_TEMPLATE_ID: "",
 
   // 接收成功回调消息的微信号，（一般来说只填自己的微信号, name填不填无所谓）
   CALLBACK_USERS: [
